6d8a465033ddd5070a4755dc09befcd6b2c2c46e
Entbugger <entbugger@github.com>
2021-03-20 09:54:44 +0100

    Test also numbers that are not perfect

--- src/test/java/ro/gal/perfectnumber/service/PerfectNumberServiceTest.java
+++ src/test/java/ro/gal/perfectnumber/service/PerfectNumberServiceTest.java
@@ -2,6 +2,7 @@ package ro.gal.perfectnumber.service;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PerfectNumberServiceTest {
@@ -9,7 +10,14 @@ public class PerfectNumberServiceTest {
     private PerfectNumberService service = new PerfectNumberService();
 
     @Test
-    public void checksNumberIsPerfect() {
+    public void detectsNumberIsPerfect() {
         assertTrue(service.isPerfectNumber(6), "6 is a valid perfect number");
+        assertTrue(service.isPerfectNumber(28), "28 is a valid perfect number");
+    }
+
+    @Test
+    public void detectsNumberIsNotPerfect() {
+        assertFalse(service.isPerfectNumber(7), "7 is not perfect number");
+        assertFalse(service.isPerfectNumber(10), "10 is not perfect number");
     }
 }
