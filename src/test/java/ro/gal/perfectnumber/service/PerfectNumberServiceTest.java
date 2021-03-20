d6c13f5ab56e3e5ed8fd7b7bdb9a24dae45eb4d9
Entbugger <entbugger@github.com>
2021-03-20 09:57:08 +0100

    Solve small IntelliJ/SonarLint issues

--- src/test/java/ro/gal/perfectnumber/service/PerfectNumberServiceTest.java
+++ src/test/java/ro/gal/perfectnumber/service/PerfectNumberServiceTest.java
@@ -5,18 +5,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class PerfectNumberServiceTest {
+class PerfectNumberServiceTest {
 
-    private PerfectNumberService service = new PerfectNumberService();
+    private final PerfectNumberService service = new PerfectNumberService();
 
     @Test
-    public void detectsNumberIsPerfect() {
+    void detectsNumberIsPerfect() {
         assertTrue(service.isPerfectNumber(6), "6 is a valid perfect number");
         assertTrue(service.isPerfectNumber(28), "28 is a valid perfect number");
     }
 
     @Test
-    public void detectsNumberIsNotPerfect() {
+    void detectsNumberIsNotPerfect() {
         assertFalse(service.isPerfectNumber(7), "7 is not perfect number");
         assertFalse(service.isPerfectNumber(10), "10 is not perfect number");
     }
