6a89ef47941a53a214b57af1f4342a7f6e17b9a6
Entbugger <entbugger@github.com>
2021-03-20 10:08:44 +0100

    Validate input to be a natural number

--- src/main/java/ro/gal/perfectnumber/service/PerfectNumberService.java
+++ src/main/java/ro/gal/perfectnumber/service/PerfectNumberService.java
@@ -9,6 +9,9 @@ public class PerfectNumberService {
      * Naive implementation of checking whether a number is perfect.
      */
     public boolean isPerfectNumber(int number) {
+        if (number<0) {
+            throw new IllegalArgumentException("Number must be positive");
+        }
         int sumOfDividers = 0;
         for (int i=1; i<number; i++) {
             if (number%i == 0) {
