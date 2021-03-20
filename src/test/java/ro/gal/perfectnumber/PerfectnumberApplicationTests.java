e7c1e8d1f968b794e436d52fd0946a29683b7116
Entbugger <entbugger@github.com>
2021-03-20 09:24:48 +0100

    Test that "check" endpoint is reachable
    This test is failing.

--- src/test/java/ro/gal/perfectnumber/PerfectnumberApplicationTests.java
+++ src/test/java/ro/gal/perfectnumber/PerfectnumberApplicationTests.java
@@ -1,13 +1,25 @@
 package ro.gal.perfectnumber;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PerfectnumberApplicationTests {
 
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
-    void contextLoads() {
+    public void isCheckNumberEndpointReachable() {
+        Boolean result = restTemplate.getForObject("http://localhost:" + port + "/perfect-number/check", Boolean.class);
+        assertThat(result).isEqualTo(false);
     }
 
 }
