6c3a36b8aec178f3e89391ca37c6cffdd9092c81
Entbugger <entbugger@github.com>
2021-03-20 11:31:38 +0100

    Unit-test generation of perfect numbers
    This test is failing.

--- src/test/java/ro/gal/perfectnumber/service/PerfectNumberServiceTest.java
+++ src/test/java/ro/gal/perfectnumber/service/PerfectNumberServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -25,5 +26,11 @@ class PerfectNumberServiceTest {
     void detectsNumberIsNotPerfect() {
         assertFalse(service.isPerfectNumber(7), "7 is not perfect number");
         assertFalse(service.isPerfectNumber(10), "10 is not perfect number");
+    }
+
+    @Test
+    void generatesPerfectNumbers() {
+        List<Long> perfectNumbers = service.generatePerfectNumbers(2, 8129);
+        assertThat(perfectNumbers).containsExactly(6L, 28L, 496L, 8128L);
     }
 }
