81469cebb35d08fea9c73893cf1ad73b2b889708
Entbugger <entbugger@github.com>
2021-03-20 10:19:16 +0100

    Iterate only to sqrt(n)
    This is an improved algorithm:
    1. start with 2, because 1 is always a divisor
    2. iterate only to sqrt(n) because we already know the other divisor

--- src/main/java/ro/gal/perfectnumber/service/PerfectNumberService.java
+++ src/main/java/ro/gal/perfectnumber/service/PerfectNumberService.java
@@ -6,16 +6,19 @@ import org.springframework.stereotype.Service;
 public class PerfectNumberService {
 
     /**
-     * Naive implementation of checking whether a number is perfect.
+     * Checks whether a number is perfect.
+     *
+     * Implementation: iterate only to sqrt(number) and add all divisors
      */
     public boolean isPerfectNumber(int number) {
         if (number<0) {
             throw new IllegalArgumentException("Number must be positive");
         }
-        int sumOfDividers = 0;
-        for (int i=1; i<number; i++) {
+        int sumOfDividers = 1;
+        for (int i=2; i*i<=number; i++) {
             if (number%i == 0) {
-                sumOfDividers += i;
+                sumOfDividers += i;//add divisor
+                sumOfDividers += number / i;//add the other divisor
             }
         }
         return sumOfDividers == number;
