e1b1444653c32445d4c8596fc236444723c5ab36
Entbugger <entbugger@github.com>
2021-03-20 09:28:22 +0100

    Add PerfectNumber controller
    This makes previous test pass.

--- src/main/java/ro/gal/perfectnumber/rest/PerfectNumberController.java
+++ src/main/java/ro/gal/perfectnumber/rest/PerfectNumberController.java
@@ -1,2 +1,15 @@
-package ro.gal.perfectnumber.rest;public class PerfectNumberController {
+package ro.gal.perfectnumber.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/perfect-number")
+public class PerfectNumberController {
+
+    @RequestMapping("/check")
+    public @ResponseBody boolean checkNumber() {
+        return false;
+    }
 }
