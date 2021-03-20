e944055353be455b64caf68fa9b651d70e3ff757
Entbugger <entbugger@github.com>
2021-03-20 09:51:45 +0100

    Check naively whether a number is perect
    This is not optimal.

--- src/main/java/ro/gal/perfectnumber/service/PerfectNumberService.java
+++ src/main/java/ro/gal/perfectnumber/service/PerfectNumberService.java
@@ -5,7 +5,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class PerfectNumberService {
 
+    /**
+     * Naive implementation of checking whether a number is perfect.
+     */
     public boolean isPerfectNumber(int number) {
-        return false;
+        int sumOfDividers = 0;
+        for (int i=1; i<number; i++) {
+            if (number%i == 0) {
+                sumOfDividers += i;
+            }
+        }
+        return sumOfDividers == number;
     }
 }
